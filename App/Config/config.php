974367af01d4409e0cf2d0f6ea2db77f22949980
mohammad <mohammad.mohseni20@yahoo.com>
2021-10-01 11:20:54 +0200

    26 config file & Autoloading

---
+++ App/Config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(__DIR__));
+    // URL Root
+    define('URLROOT', 'http://localhost:8080/webprogmvc/');
+    // Site Name
+    define('SITENAME', 'WebProgMVC');
