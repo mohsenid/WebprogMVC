c6456c2e01eacacaea6324c005e6b9656bc5566c
mohammad <mohammad.mohseni20@yahoo.com>
2021-10-01 10:57:53 +0200

    25 views

--- App/Controllers/Pages.php
+++ App/Controllers/Pages.php
@@ -1,10 +1,18 @@
 <?php
-    class Pages{
+    class Pages extends Controller{
         public function __construct()
         {
-            echo 'Pages Load';
+            // echo 'Pages Load';
         }
         public function index(){
-            
+            // $articles = $this->model();
+            $data = [
+                'title' => 'Webprog.ir' ,
+                // 'article' => $articles
+            ];
+            $this->view('Pages/index', $data);
+        }
+        public function about(){
+            $this->view('Pages/about');
         }
     }
