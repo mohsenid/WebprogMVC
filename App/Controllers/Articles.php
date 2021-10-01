1161089f9765f14dc52fc2250f3e1c8078b99d75
mohammad <mohammad.mohseni20@yahoo.com>
2021-10-01 09:29:36 +0200

    24 Base controller

--- App/Controllers/Articles.php
+++ App/Controllers/Articles.php
@@ -1,13 +1,14 @@
 <?php
-    class Articles{
+    class Articles extends Controller{
         public function __construct()
         {
-            echo 'Article Load';
+            // echo 'Article Load';
         }
         public function index(){
-            
+            // $this->model('article');
+            $this->view('article');
         }
         public function edit($id){
-            echo $id;
+            echo $id; 
         }
     }
