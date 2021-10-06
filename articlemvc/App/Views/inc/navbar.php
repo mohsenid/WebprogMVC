03c3c9fe821a9b0d8d03c0cf20d1da165a757346
mohammad <mohammad.mohseni20@yahoo.com>
2021-10-06 08:58:49 +0200

    33 User controller

--- articlemvc/App/Views/inc/navbar.php
+++ articlemvc/App/Views/inc/navbar.php
@@ -1,13 +1,13 @@
 <nav class="masthead mb-auto" >
     <div>
-        <a href="<?php echo URLROOT; ?>"/pages/index>
+        <a href="<?php echo URLROOT; ?>/pages/index">
             <h4 class="masthead-brand text-center text-white" >
                 <?php echo SITENAME ; ?>
             </h4>
         </a>
         <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="#">Login</a>
-            <a class="nav-link " href="#">Register</a>
+            <a class="nav-link active" href="<?php echo URLROOT; ?>users/login">Login</a>
+            <a class="nav-link " href="<?php echo URLROOT; ?>users/register">Register</a>
         </nav>
     </div>
 
